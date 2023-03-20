490bfb26e630bcf045f4b7c033f7701def0703a5
Daniel Lee <daniel8lee58@gmail.com>
2023-03-19 23:21:49 -0500

    edits

--- client/src/api/url.ts
+++ client/src/api/url.ts
@@ -1,2 +1,2 @@
 export const apiUrl =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:4000' : 'https://muddy-brook-9047.fly.dev/'
+  process.env.NODE_ENV == 'development' ? 'http://localhost:4000' : 'https://muddy-brook-9047.fly.dev'
